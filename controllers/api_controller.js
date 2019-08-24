e621902c5b9dcdb4d5e7ff1aec5d274117ed2c44
Mo <Gabalyma@gmail.com>
2019-08-24 13:56:25 -0700

    new dump data

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -15,9 +15,10 @@ exports.fetch = function(req, res) {
 
 const boats = [
   {
-    thumbnail: "",
-
-    image: "",
+    thumbnail:
+      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=734&q=80",
+    image:
+      "https://images.unsplash.com/photo-1563296291-14f26f10c20f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80",
 
     name: "X3OATS",
 
@@ -28,9 +29,11 @@ const boats = [
     price: "70"
   },
   {
-    thumbnail: "",
+    thumbnail:
+      "https://images.unsplash.com/photo-1476493279419-b785d41e38d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
 
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1556766986-b427ed708496?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
 
     name: "Shark Lover",
 
@@ -41,9 +44,11 @@ const boats = [
     price: "30"
   },
   {
-    thumbnail: "",
+    thumbnail:
+      "https://images.unsplash.com/photo-1500832333538-837287aad2b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
 
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1563296291-4ffce652738f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1790&q=80",
 
     name: "Life On The Line",
 
@@ -54,9 +59,11 @@ const boats = [
     price: "50"
   },
   {
-    thumbnail: "",
+    thumbnail:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
 
-    image: "",
+    image:
+      "https://images.unsplash.com/photo-1527431293370-0cd188ca5d15?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80",
 
     name: "Live On the Wave",
 
