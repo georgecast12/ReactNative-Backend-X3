0145941b2e7ae86529af9940686a94cf203a6553
Mo <Gabalyma@gmail.com>
2019-08-26 18:32:54 -0700

    Batch2Boats

--- controllers/api_controller.js
+++ controllers/api_controller.js
@@ -14,79 +14,79 @@ exports.fetch = function(req, res) {
 };
 
 const boats = [
-  {
-    thumbnail:
-      "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=734&q=80",
-    image:
-      "https://images.unsplash.com/photo-1563296291-14f26f10c20f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80",
-
-    name: "X3OATS",
-
-    location: "Santa Monica",
-
-    rents: "50",
-
-    price: "70"
-  },
-  {
-    thumbnail:
-      "https://images.unsplash.com/photo-1476493279419-b785d41e38d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
-
-    image:
-      "https://images.unsplash.com/photo-1556766986-b427ed708496?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
-
-    name: "Shark Lover",
-
-    location: "Venice Beach",
-
-    rents: "10",
-
-    price: "30"
-  },
-  {
-    thumbnail:
-      "https://images.unsplash.com/photo-1500832333538-837287aad2b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
-
-    image:
-      "https://images.unsplash.com/photo-1563296291-4ffce652738f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1790&q=80",
-
-    name: "Life On The Line",
-
-    location: "Santa Monica",
-
-    rents: "12",
-
-    price: "50"
-  }
   // {
   //   thumbnail:
-  //     "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
-
+  //     "https://images.unsplash.com/photo-1497551060073-4c5ab6435f12?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=734&q=80",
   //   image:
-  //     "https://images.unsplash.com/photo-1527431293370-0cd188ca5d15?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80",
+  //     "https://images.unsplash.com/photo-1563296291-14f26f10c20f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1789&q=80",
 
-  //   name: "Live On the Wave",
+  //   name: "X3OATS",
 
-  //   location: "San Diego",
+  //   location: "Santa Monica",
 
-  //   rents: "5",
+  //   rents: "50",
 
-  //   price: "100"
+  //   price: "70"
   // },
   // {
   //   thumbnail:
-  //     "https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80",
+  //     "https://images.unsplash.com/photo-1476493279419-b785d41e38d8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80",
+
   //   image:
-  //     "https://images.unsplash.com/photo-1562812179-23b8920689a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2132&q=80",
+  //     "https://images.unsplash.com/photo-1556766986-b427ed708496?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80",
 
-  //   name: "Dreamer",
+  //   name: "Shark Lover",
 
-  //   location: "Marina Del Rey",
+  //   location: "Venice Beach",
 
-  //   rents: "70",
+  //   rents: "10",
 
-  //   price: "25"
+  //   price: "30"
+  // },
+  // {
+  //   thumbnail:
+  //     "https://images.unsplash.com/photo-1500832333538-837287aad2b6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+
+  //   image:
+  //     "https://images.unsplash.com/photo-1563296291-4ffce652738f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1790&q=80",
+
+  //   name: "Life On The Line",
+
+  //   location: "Santa Monica",
+
+  //   rents: "12",
+
+  //   price: "50"
   // }
+  {
+    thumbnail:
+      "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=668&q=80",
+
+    image:
+      "https://images.unsplash.com/photo-1527431293370-0cd188ca5d15?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1651&q=80",
+
+    name: "Live On the Wave",
+
+    location: "San Diego",
+
+    rents: "5",
+
+    price: "100"
+  },
+  {
+    thumbnail:
+      "https://images.unsplash.com/photo-1531251445707-1f000e1e87d0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=581&q=80",
+    image:
+      "https://images.unsplash.com/photo-1562812179-23b8920689a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2132&q=80",
+
+    name: "Dreamer",
+
+    location: "Marina Del Rey",
+
+    rents: "70",
+
+    price: "25"
+  }
 ];
 
 exports.addboat = function(req, res) {
